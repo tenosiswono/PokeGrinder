47e62153d8ad5e93dbcbff081372f22c18d8e712
Xombie <71486556+Mehul343@users.noreply.github.com>
2021-12-20 19:41:35 +0530

    Update captcha.py

--- lib/captcha.py
+++ lib/captcha.py
@@ -3,7 +3,7 @@ from python3_anticaptcha import ImageToTextTask
 async def anticaptcha(image):
     print("Solving captcha...")
     
-    ANTICAPTCHA_KEY = "43485a97fc151a8cd654141a46cdaea2"
+    ANTICAPTCHA_KEY = conf.general.anticaptcha_token
 
     user_answer = ImageToTextTask.ImageToTextTask(anticaptcha_key = ANTICAPTCHA_KEY).\
         captcha_handler(captcha_link=image)
